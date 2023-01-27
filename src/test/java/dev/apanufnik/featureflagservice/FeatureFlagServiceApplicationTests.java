dfea791aac6e42020f9dd0ec2ea498fa70a87639
andrzej.panufnik <andrzej.panufnik@orange.com>
2023-01-27 18:40:27 +0100

    initialize repository

---
+++ src/test/java/dev/apanufnik/featureflagservice/FeatureFlagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.apanufnik.featureflagservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
