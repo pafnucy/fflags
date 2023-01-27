2e9525f0007dbcc504220da35c55300f309c0a2b
andrzej.panufnik <andrzej.panufnik@orange.com>
2023-01-27 20:07:06 +0100

    fix initial test setup
    want to load whole context

--- src/test/java/dev/apanufnik/featureflagservice/FeatureRetrievalSecurityTest.java
+++ src/test/java/dev/apanufnik/featureflagservice/FeatureRetrievalSecurityTest.java
@@ -3,8 +3,8 @@ package dev.apanufnik.featureflagservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
-@WebMvcTest(FeaturePermissionController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class FeatureRetrievalSecurityTest {
 
     @Autowired
